4ebfc40dc41ae6b1f38ca77f1b8cb4a410b7dc98
Beau Elliott <belliott1516@gmail.com>
2022-05-06 15:58:30 -0700

    completed conditional on harrypottercharacer

--- src/HarryPotterCharacter.js
+++ src/HarryPotterCharacter.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 export default function HarryPotterCharacter({ character }) {
   return (
-    <div style={{ background: character.house === 'Gryffindor' ? 'crimson' : 'blue' }} className='harry-potter-card' >
+    <div style={{ background: character.house === 'Gryffindor' && 'crimson' || 
+                              character.house === 'Slytherin' && 'green' ||
+                              character.house === 'Ravenclaw' && 'blue' }} 
+    className='harry-potter-card' >
       <h1>{character.name}</h1>
-      <h4>{character.house}</h4>
-      <p>{character.wand_material}</p>
-      <p>{character.wand_length}</p>
-      <p>{character.wand_core}</p>
+      <h4>House:{character.house}</h4>
+      <p>Wand Material: {character.wand_material}</p>
+      <p>Length: {character.wand_length}</p>
+      <p>Core: {character.wand_core}</p>
       <ul>
         {character.appearance_in_books.map((books, i) => <li key={books + i}>{books}</li>)}
       </ul>
