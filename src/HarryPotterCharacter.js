e1699c25ef4c6645e654dd28a7792b6fe9d464e9
Beau Elliott <belliott1516@gmail.com>
2022-05-06 15:17:56 -0700

    adjusted turnery in harrypottercharacter.js

--- src/HarryPotterCharacter.js
+++ src/HarryPotterCharacter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function HarryPotterCharacter({ character }) {
   return (
-    <div style={{ background: 'gray' }} className='harry-potter-card' >
+    <div style={{ background: character.house === 'Gryffindor' ? 'crimson' : 'blue' }} className='harry-potter-card' >
       <h1>{character.name}</h1>
       <h4>{character.house}</h4>
       <p>{character.wand_material}</p>
